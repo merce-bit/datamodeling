2bef57e9bb7b22fb552136a149b1c60158ef933c
nila.autar@outlook.com <nila.autar@outlook.com>
2023-12-18 11:05:41 +0000

    Snapshot sales 18 dec

--- models/Silver/DIM_SALESPERSON_PURCHASER.sql
+++ models/Silver/DIM_SALESPERSON_PURCHASER.sql
@@ -1,6 +1,7 @@
 SELECT
 "code"                        as "CODE"
 ,md5 (concat("code", "Name")) as "_surrogate_key"
+,(concat("code", "_", "Name")) as "_surrogate_key2"
 ,"Name"                       as "NAME"
 ,"E-Mail"                     as "E-MAIL"
 ,"Phone No_"                  as "PHONE NO"
---
+++ snapshots/DIM_CUSTOMER_HIST.sql
@@ -0,0 +1,29 @@
+{% snapshot DIM_CUSTOMER_HIST %}
+
+SELECT 
+   no_ as "CUSTOMER ID"
+, "Name"
+, address
+, "Address 2"
+, city
+, contact
+, "Phone No_"
+, "Territory Code"
+, "Chain Name"
+, "Customer Posting Group"
+, "Currency Code"
+, "Customer Price Group"
+, "Language Code"
+, "Statistics Group"
+, "Payment Terms Code"
+, "Fin_ Charge Terms Code"
+, "Salesperson Code"
+, "Shipment Method Code"
+, "Shipping Agent Code"
+, "Place of Export"
+, "Invoice Disc_ Code"
+, "Customer Disc_ Group"
+, "Country_Region Code"
+from {{source('raw', 'nav9_sl_customer')}} 
+
+{% endsnapshot %}
